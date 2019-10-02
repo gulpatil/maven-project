9e364349c6bd7015bcdee98e00c650f1d7058c9c
EC2 Default User <ec2-user@ip-172-31-8-126.ap-south-1.compute.internal>
2019-10-02 17:28:51 +0000

    update for error correction in deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -5,6 +5,6 @@ echo $1 >> /tmp/.auth
 echo $2 >> /tmp/.auth
 
 
-scp -i /opt/prod /tmp/.auth prod-user@13.126.7.8:/tmp/.auth
-scp -i /opt/prod ./jenkins/deploy/publish.sh prod-user@13.126.7.8:/tmp/publishfile.sh
+scp -i /opt/prod /tmp/.auth prod-user@13.127.127.254:/tmp/.auth
+scp -i /opt/prod ./jenkins/deploy/publish.sh prod-user@113.127.127.254:/tmp/publishfile.sh
 ssh -i /opt/prod prod-user@13.127.127.254 "/tmp/publishfile.sh"
