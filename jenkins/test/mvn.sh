7f0251112808c0e457e1d196a0e5ba62ead3af68
EC2 Default User <ec2-user@ip-172-31-8-126.ap-south-1.compute.internal>
2019-10-02 05:53:53 +0000

    Fix for PWD library

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -4,5 +4,7 @@ echo "*****************************************"
 echo "*******Testing the code *****************"
 echo "*****************************************"
 
-docker run --rm -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+WORKSPACE=/home/ec2-user/jenkins/jenkins_home/workspace/Pipeline-maven
+
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
 
